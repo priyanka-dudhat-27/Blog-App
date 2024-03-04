da5de276875da9c06ba7eaf25bd65a5a1bd5d6b0
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-03-04 12:46:21 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv').config()
+const port=process.env.Port || 3000
+const dbConnection=require('./config/db')
+const userRouter=require('./routes/user_routes')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api/users',userRouter)
+
+dbConnection();
+app.listen(port,(err)=>{
+    (err)?console.log('something wrong'):console.log(`server is running on port ${port}`)
+})
